74e009981e1761d304e1bece3ed7cf3d01e47617
om65praj <om65praj@gmail.com>
2017-10-18 15:50:51 +0530

    modification at 3:50

---
+++ AngularPro/js/gambler.js
@@ -0,0 +1,41 @@
+/********************************************************************************
+   
+    Description : Gambler
+
+
+
+   @auther : Sushilkumar Bobade	
+   @Date   : 18.10.2017
+
+**********************************************************************************/
+
+function gamFunCall(){
+
+        var $Stake=parseInt(document.getElementById("stake").value);
+        var $Goal=parseInt(document.getElementById("goal").value);
+        var $nTimes=parseInt(document.getElementById("ntime").value);
+         
+     
+alert("hiii");
+
+            var bets=0;
+             for(var i=0; i<$nTimes; i++) 
+		{ alert("inloop");
+		/*	var cash = $Stake;
+	            
+			while (cash > 0 && cash < $Goal)
+			{
+				bets++;
+	        
+				if (Math.random() < 0.5) 
+					cash++;     // win $1
+				else                     
+					cash--;     // lose $1
+			}
+	            if (cash == $Goal) 
+	            	wins++;         */
+
+}
+
+}
+
